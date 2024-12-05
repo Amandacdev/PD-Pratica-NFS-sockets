bdfd771c7e9383927627aa08e798351937453507
Amanda Cruz de Araújo <amanda.crz.aj@gmail.com>
2024-12-05 18:26:40 -0300

    Add files via upload

---
+++ src/br/edu/ifpb/gugawag/so/sockets/Cliente2.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.gugawag.so.sockets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente2 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("== Cliente ==");
+
+
+        Socket socket = new Socket("127.0.0.1", 7001);
+  
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        Scanner teclado = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Lista de comandos: readdir | create | rename | remove | exit");
+            System.out.print("Digite o comando: ");
+            String comando = teclado.nextLine();
+            dos.writeUTF(comando);
+
+            String resposta = dis.readUTF();
+            System.out.println("Servidor respondeu: " + resposta);
+        }
+
+    }
+}
